208eb6aa9b7b11e313bc7767267c3d3f871a53b0
Uliana <kchopin1@gmail.com>
2026-05-09 02:30:19 +0200

    database, models, 1st controller

---
+++ Models/Collection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApi.Models;
+
+public partial class Collection
+{
+    public int Id { get; set; }
+
+    public int? UserId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public bool? IsPublic { get; set; }
+
+    public virtual ICollection<Object> Objects { get; set; } = new List<Object>();
+
+    public virtual ICollection<StorageLocation> StorageLocations { get; set; } = new List<StorageLocation>();
+
+    public virtual User? User { get; set; }
+}
