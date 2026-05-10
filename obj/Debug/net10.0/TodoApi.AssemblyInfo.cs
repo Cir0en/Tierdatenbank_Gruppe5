dd0a8fa871ed8742c935cdc6b60a1f2b1cdc20c8
Cir0en <lukas.waeschenbach@gmail.com>
2026-05-10 19:38:52 +0200

    Gitignore

--- obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73e78d35b93ed69097fce17cbc98bcf8e885caf4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db96e8a38f0d182d2498d294360a3370fef74a73")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
